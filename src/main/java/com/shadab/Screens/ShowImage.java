862e9c2b27b65dfc29d6ad662c433ff247c2bca0
shadabdsw <shadab1996@gmail.com>
2021-08-29 12:54:12 +0530

    Image on Frame

---
+++ src/main/java/com/shadab/Screens/ShowImage.java
@@ -0,0 +1,35 @@
+package com.shadab.Screens;
+
+import javax.imageio.ImageIO;
+import javax.swing.JLabel;
+import org.json.JSONObject;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.net.URL;
+import java.awt.Image;
+
+public class ShowImage extends JLabel {
+    public ShowImage(JSONObject obj) {
+        String imgUrl = obj.getString("graphql/user/profile_pic_url_hd");
+        try {
+            // System.out.println(imgUrl);
+            URL url = new URL(imgUrl);
+            Image image = ImageIO.read(url);
+            image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+
+            this.setBounds(20, 230, 600, 300);
+            this.setIcon(new ImageIcon(image));
+            this.setText("Name : " + obj.getString("logging_page_id"));
+
+            this.setFont(new Font("Aerial", Font.PLAIN, 18));
+            this.setForeground(Color.white);
+            this.repaint();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.setLayout(null);
+    }
+}
